275c9336a9697e52c99d77b769915be54e34c1c2
mpomery <bob_george33@hotmail.com>
2014-04-07 18:21:45 +0800

    More Modular!
    Still need things to test though...

--- lab01/test.py
+++ lab01/test.py
@@ -1,24 +1,34 @@
-from lcs_test import *
 import time
+import sys
 
-mod = __import__(fname, globals())
+from lcs_test import *
 
-print mod
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		print "Usage: " + str(sys.argv[0]) + " <testfile>"
+	else:
+		testfile = __import__(sys.argv[1], globals())
+		mod = __import__(testfile.fname, globals())
 
-for function in functions:
-	func = getattr(mod, function)
-	success = 0
-	failure = 0
-	starttime = time.clock()
-	for data in dataset:
-		value = func(*data[0])
-		if value == data[1]:
-			success += 1
-		else:
-			failure += 1
-	endtime = time.clock()
-	print "Test Of " + function
-	print "Successes: " + str(success)
-	print "Failures:  " + str(failure)
-	print "Accuracy:  " + str(success / (success + failure) * 100) + "%"
-	print "Time :     " + str(endtime - starttime) + "s"
+		for function in testfile.functions:
+			func = getattr(mod, function)
+			success = 0
+			failure = 0
+			print "Test Of " + function
+			starttime = time.clock()
+			for data in testfile.dataset:
+				value = func(*data[0])
+				if value == data[1]:
+					success += 1
+				else:
+					failure += 1
+					print "Failure " + data
+					print value
+					print ""
+			endtime = time.clock()
+			print ""
+			print "Successes: " + str(success)
+			print "Failures:  " + str(failure)
+			print "Accuracy:  " + str(success / (success + failure) * 100) + "%"
+			print "Time :     " + str(endtime - starttime) + "s"
+			print "-" * 40
