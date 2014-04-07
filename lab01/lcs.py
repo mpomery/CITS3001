de94d27e6e8b4d06e46e98dc0a2b50c9c618a1d2
mpomery <bob_george33@hotmail.com>
2014-04-07 15:13:37 +0800

    Untested Naive LCS

--- lab01/lcs.py
+++ lab01/lcs.py
@@ -4,9 +4,18 @@ if __name__ == '__main__':
 	print("returns the length of the LCS")
 
 def lcs_naive(string1, string2):
-	if len(string1) == 0 or len(string2) == 0:
-		return 0
-	else if len(string1) == len(string2):
-		pass
+	if len(string1) * len(string2) == 0:
+		# One string is length 0, so LCS is empty
+		return ""
+	elif string1[-1] == string2[-1]:
+		# Last characters are the same
+		print(string1[0:-1])
+		print(string2[0:-1])
+		return lcs_naive(string1[0:-1], string2[0:-1]) + string1[-1]
 	else:
-		pass
+		left = lcs_naive(string1[0:-1], string2)
+		right = lcs_naive(string1[0:-1], string2)
+		if (len(left) > len(right)):
+			return left
+		else:
+			return right
