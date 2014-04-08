756d48cc9f81b68f1b2582a6aaf734398fcf88f6
Mitchell Pomery <bob_george33@hotmail.com>
2014-04-08 11:10:56 +0800

    Test Function
    So More tests can be run at once

--- test.py
+++ test.py
@@ -3,6 +3,30 @@ import sys
 import os
 import inspect
 
+def runtest(functions, dataset):
+	for function in functions:
+		func = getattr(mod, function)
+		success = 0
+		failure = 0
+		print "Test Of " + function
+		starttime = time.clock()
+		for data in dataset:
+			value = func(*data[0])
+			if value in data[1]:
+				success += 1
+			else:
+				failure += 1
+				print "Failure " + str(data)
+				print str(value)
+				print ""
+		endtime = time.clock()
+		print ""
+		print "Successes: " + str(success)
+		print "Failures:  " + str(failure)
+		print "Accuracy:  " + str(success / (success + failure) * 100) + "%"
+		print "Time :     " + str(endtime - starttime) + "s"
+		print "-" * 40
+
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
 		print "Usage: " + str(sys.argv[0]) + " <directory> <testfile>"
@@ -12,26 +36,4 @@ if __name__ == '__main__':
 		
 		testfile = __import__(sys.argv[2], globals())
 		mod = __import__(testfile.fname, globals())
-
-		for function in testfile.functions:
-			func = getattr(mod, function)
-			success = 0
-			failure = 0
-			print "Test Of " + function
-			starttime = time.clock()
-			for data in testfile.dataset:
-				value = func(*data[0])
-				if value in data[1]:
-					success += 1
-				else:
-					failure += 1
-					print "Failure " + str(data)
-					print str(value)
-					print ""
-			endtime = time.clock()
-			print ""
-			print "Successes: " + str(success)
-			print "Failures:  " + str(failure)
-			print "Accuracy:  " + str(success / (success + failure) * 100) + "%"
-			print "Time :     " + str(endtime - starttime) + "s"
-			print "-" * 40
+		runtest(testfile.functions, testfile.dataset)
