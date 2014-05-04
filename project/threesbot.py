59340aab47a745bd8ed3bea9ce27fd10f2305926
mpomery <bob_george33@hotmail.com>
2014-05-04 23:42:14 +0800

    Mostly working movement

--- project/threesbot.py
+++ project/threesbot.py
@@ -14,17 +14,17 @@ def main():
 		return 1
 	else:
 		# This stuff is Pythonic!
-		nexttile = []
+		tiles = []
 		input = open(sys.argv[1], "r")
 		input.readline() # First 2 lines are comments
 		input.readline()
 		board = [map(int, input.readline().strip().split()) for i in range(4)]
 		printboard(board)
 		for line in input:
-			map(nexttile.append, map(int, line.strip().split()))
+			map(tiles.append, map(int, line.strip().split()))
 		input.close()
 		print("score: " + str(scoreboard(board)))
-		play(board, nexttile)
+		play(board, tiles)
 
 def getxy(x, y, udlr):
 	# Pretends we have rotated the board so a movement left means a movement
@@ -37,10 +37,33 @@ def getxy(x, y, udlr):
 		x, y = y, x
 	return (x, y)
 
-def domove(board, move):
+def domove(board, move, nexttile):
 	# Treat everything as a left shift. Just "rotate" values to change them
 	# We can do this with the magic of getyx()!
 	# Which runs in O(1) time and O(1) memory to be super cool!
+	for i in range(4):
+		# everything moves "left". deal with it row by row
+		# start is second position
+		for j in range(4):
+			x, y = getxy(i, j, move)
+			xn, yn = getxy(i, j + 1, move)
+			if max(x, y, xn, yn) != 4:
+				#print(move + str(i) + str(j) + str(x) + str(y) + str(xn) + str(yn))
+				if board[x][y] == board[xn][yn]:
+					# equal. combine them
+					board[x][y] += board[xn][yn]
+					board[xn][yn] = 0
+				elif board[x][y] == 0:
+					# this square is empty. try fill it.
+					board[x][y] += board[xn][yn]
+					board[xn][yn] = 0
+					# possibly merge these last two...
+				elif sorted((board[x][y], board[xn][yn])) == (1, 2):
+					#Merge them again!
+					board[x][y] += board[xn][yn]
+					board[xn][yn] = 0
+					# We could combine these all!
+	
 	return board
 
 def printboard(board):
@@ -64,7 +87,15 @@ def scoreboard(board):
 	return score
 
 def play(board, tiles):
-	pass
+	moves = "UDLR"
+	for i in range(len(moves)):
+		move = moves[i]
+		tile = tiles[i]
+		board = domove(board, move, tile)
+		print(move)
+		printboard(board)
+		print("score: " + str(scoreboard(board)))
+		print('')
 
 if __name__ == '__main__':
 	main()
