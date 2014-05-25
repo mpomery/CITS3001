b01273f1b3a77f8240ee89a7130bbbff242ed441
mpomery <bob_george33@hotmail.com>
2014-05-25 20:28:43 +0800

    Writes Output to a file

--- project/code/python/threesbot.py
+++ project/code/python/threesbot.py
@@ -24,7 +24,6 @@ def timing(f):
 	return wrap
 
 # Main function. Loads the input file
-@timing
 def main():
 	# Load Data from input file arg
 	# We assume the input is correctly formatted
@@ -46,75 +45,47 @@ def main():
 		for line in input:
 			map(tiles.append, map(int, line.split()))
 		input.close()
-		for i in range (100):
+		
+		for i in range(1):
 			boardout = copy.deepcopy(board)
 			(moves, finalboard) = naive(boardout, tiles)
-			threes.printboard(finalboard)
-			print(moves)
-			print("")
-			print("")
+			if i is 0:
+				threes.printboard(finalboard)
+				print("Score: " + str(threes.scoreboard(finalboard)))
+				print(moves)
+				output = open(sys.argv[2], "w")
+				output.write("\n")
+				output.write("\n")
+				output.write(moves)
 
-# The bot itself
+# Niave bot. Will find the best move then make it.
+@timing
 def naive(board, tiles):
 	i = 0
 	output = ""
-	#print(tiles)
-	#raw_input()
 	while i < len(tiles):
-		#if i >= 48:
-		#	raw_input()
-		#print(board)
 		qt = QuinaryTree.QuinaryTree(board)
-		#qt.printqt()
 		qt.makeleaves(tiles[i])
-		#print(i)
-		#print(len(tiles))
 		
-		if qt.left.board == None and qt.right.board == None and qt.up.board == None and qt.down.board == None:
-			#print("Nones!")
+		if qt.left.board == None and qt.right.board == None and \
+		qt.up.board == None and qt.down.board == None:
 			return(output, board)
 		
 		maximum = max(qt.left.score, qt.right.score, qt.up.score, qt.down.score)
-		
 		if (maximum == qt.left.score):
-			#print("Moved: Left")
 			board = qt.left.board
-			#print(board)
 			output += "L"
 		elif (maximum == qt.right.score):
-			#print("Moved: Right")
 			board = qt.right.board
-			#print(board)
 			output += "R"
 		elif (maximum == qt.up.score):
-			#print("Moved: Up")
 			board = qt.up.board
-			#print(board)
 			output += "U"
 		elif (maximum == qt.down.score):
-			#print("Moved: Down")
 			board = qt.down.board
-			#print(board)
 			output += "D"
 		i += 1
-		#print("")
-	#print("")
-	#print("")
-	#print(output)
 	return(output, board)
-	
-	"""moves = "UDRRRRUUDDDRLLLUUDDULRLUDDRRRDRUUULLUUURRRUURULLLLURULUULULURUUURUULULURUR"
-	for i in range(len(moves)):
-		print("Move: " + str(i + 1))
-		move = moves[i]
-		tile = tiles[i]
-		board = threes.domove(board, move, tile)
-		print(move)
-		threes.printboard(board)
-		print("added tile: " + str(tiles[i]))
-		print("score: " + str(threes.scoreboard(board)))
-		print('')
-		#raw_input()"""
 
 # If called from the command line, run main
 if __name__ == '__main__':
